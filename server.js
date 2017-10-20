6495ba4a042da2428a76d39f56d88f1675dc48f8
Russell Barthel <russell.barthel@gmail.com>
2017-10-20 00:14:36 +0000

    add .gitignore & .babelrc

---
+++ server.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+      ignored: /node_modules/
+    }
+  })
+  .listen(3000, '0.0.0.0', function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+
+    console.log('Running at http://0.0.0.0:3000');
+  });
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+
 module.exports = {
   entry: './src/index.jsx',
   // https://webpack.js.org/configuration/output/

006b4b15a1c7623627adc449c9dcfb134dcf5ae1
BugleJones <spencerjhamilton@gmail.com>
2017-10-21 21:39:29 +0000

    Cleaned up video banner

--- src/HomePage/HomePage_Banner.jsx
+++ src/HomePage/HomePage_Banner.jsx
@@ -3,20 +3,24 @@ import React, {Component} from 'react';
 class HomePage_Banner extends Component {
   render() {
     return (
-      <div className="hero-slide">
-        <div className="hero-slide homePageBanner">
+      <div>
+        <div className="homePageBanner">
           <div className="clip">
-            <video className="hero-slide video" autoPlay loop muted style={{zIndex:98}}>
+            <video className="video" autoPlay loop muted style={{zIndex:98}}>
               <source src="https://d3mlfyygrfdi2i.cloudfront.net/Assemble_Hero_Loop_desktop.mp4" type="video/mp4"/>
               <source src="https://d3mlfyygrfdi2i.cloudfront.net/Assemble_Hero_Loop_webm.webm" type="video/webm"/>
               <img alt="" src="https://d3mlfyygrfdi2i.cloudfront.net/Screen_Shot_2017-09-11_at_3.57.10_PM.png"/>
               You need an HTML5 enabled browser to view this video.
             </video>
           </div>
-          <form className="form-inline my-2 my-lg-0 homePageBannerSearch">
+          <div className="homePageBannerSearch">
+            <h1 className="search-title">Experience Food as an Art Form</h1>
+            <form className="form-inline my-2 my-lg-0">
             <input className="form-control mr-sm-2 homePageBannerSearchBox" type="text" placeholder="Search" aria-label="Search"></input>
             <button className="btn btn-outline-success my-2 my-sm-0 homePageBannerSearchButton" type="submit">Search</button>
           </form>
+          </div>
+          
         </div>
       </div>
     );

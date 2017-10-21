d3bd5409ebc39c73da3db7db77dfbeae85f0523a
BugleJones <spencerjhamilton@gmail.com>
2017-10-21 00:32:10 +0000

    Added multiple packages related to knex to connect to the database, updated index.js on the server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
+"use strict";
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
 const express = require('express');
+const knex = require("knex")(knexConfig[ENV]);
+const bodyParser = require("body-parser");
+
+const knexConfig = require("./knexfile");
+const knex = require("knex")(knexConfig[ENV]);
+const morgan = require("morgan");
+const knexLogger = require("knex-logger");
 
 const app = express();
+
+app.use(knexLogger(knex));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'))
 

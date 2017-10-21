ffa38ebe7ed69c214c2817762c9cfd927e490916
TuanPham303 <phamcongtuan303@gmail.com>
2017-10-21 16:34:31 -0700

    clean up index.jsx file

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap';
 import 'jquery';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+
 import HomePage from './HomePage/HomePage.jsx';
 import EventPage from './EventPage/EventPage.jsx';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import UserProfile from './UserProfile/UserProfile.jsx';
+
 
 ReactDOM.render((
   <Router>
     <div>
       <Route exact path="/" component={HomePage}/>
       <Route path="/event" component={EventPage}/>
+      <Route path="/user" component={UserProfile}/>
     </div>
   </Router>
 ), document.getElementById('react-root'));

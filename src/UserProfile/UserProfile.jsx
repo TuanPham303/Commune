6f55796261bdb31f0a370080a2469b5abd72ae82
TuanPham303 <phamcongtuan303@gmail.com>
2017-10-24 15:39:36 -0700

    commit before pulling

--- src/UserProfile/UserProfile.jsx
+++ src/UserProfile/UserProfile.jsx
@@ -4,6 +4,29 @@ import UserProfile_Header from './UserProfile_Header.jsx';
 import UserProfile_Reviews from './UserProfile_Reviews.jsx';
 
 class UserProfile extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentDidMount(){
+    const userDetail = () => {
+      $.ajax({
+        method: "GET",
+        url: "/api/users",
+        success: data => {
+          console.log(data);
+          // this.setState({
+          //   previewEvents: this.state.previewEvents.concat(data)
+          // })
+        }
+      })
+    }
+    userDetail();
+  }
+
   render() {
     return (
       <div className="wrapper">

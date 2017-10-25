a0215e04545e5f523da61a8e18a1a273b7047d07
Scotty <scott.hagan@aol.com>
2017-10-25 18:48:52 +0000

    changed seed users to have a hashed password and working on login in form making post request

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,15 +1,37 @@
 import React, {Component} from 'react';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
 
   handleLogin = event => {
+
+    console.log(event);
+    event.preventDefault();
     $.ajax({
-      method: "GET",
+      method: "POST",
       url: "/api/users/login",
       success: data => {
         console.log(data);
   
       }
+    })
+  }
+
+  emailChange = event => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  passwordChange = event => {
+    this.setState({
+      password: event.target.value
     })
   }
   
@@ -26,16 +48,16 @@ class Login extends Component {
               </button>
             </div>
             <div className="modal-body">
-              <form>
+              <form onSubmit={ this.handleLogin }>
                 <div className="form-group">
                   <label for="loginEmail">EMAIL</label>
-                  <input type="email" className="form-control" id="loginEmail" placeholder="email@example.com"></input>
+                  <input type="email" className="form-control" id="loginEmail" placeholder="email@example.com" value ={this.state.email} onChange={this.emailChange}></input>
                 </div>
                 <div className="form-group">
                   <label for="loginPassword">PASSWORD</label>
-                  <input type="password" className="form-control" id="loginPassword" placeholder="Password"></input>
+                  <input type="password" className="form-control" id="loginPassword" placeholder="Password" value ={this.state.password} onChange={this.passwordChange}></input>
                 </div>
-                <button type="submit" className="btn btn-primary" onSubmit={ this.handleLogin }>Login</button>
+                <button type="submit" className="btn btn-primary" >Login</button>
               </form>
             </div>
           </div>

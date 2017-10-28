1d592feb0739afd9f33f3f3e5590cfa3b21d387b
Russell Barthel <russell.barthel@gmail.com>
2017-10-28 23:18:06 +0000

    google javascript api key no longer hard coded

--- src/EventPage/EventPage_Map.jsx
+++ src/EventPage/EventPage_Map.jsx
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
- 
+
 const AnyReactComponent = ({ text }) => <div style={{
     position: 'relative', color: 'white', background: 'blue', opacity: '0.5',
-    height: 100, width: 100, top: -20, left: -30, borderRadius: '50%'   
+    height: 80, width: 80, top: -20, left: -30, borderRadius: '50%'
   }}></div>;
- 
+
 export default class EventPage_Map extends Component {
   state = {
     center: {lat: this.props.location.x, lng: this.props.location.y},
-    zoom: 11
+    zoom: 13
   };
- 
+
   render() {
     return (
       <GoogleMapReact
-        bootstrapURLKeys={{key: "AIzaSyBKKwK2AfjqhjmwQKaUvWxXwFOVMY_rgn4"}}
+        bootstrapURLKeys={{key: this.props.googleMapKey}}
         defaultCenter={this.state.center}
         defaultZoom={this.state.zoom}
       >

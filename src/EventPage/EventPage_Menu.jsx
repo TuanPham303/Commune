4552c54e267aafac78b6d594ffe159f80bd9928e
Russell Barthel <russell.barthel@gmail.com>
2017-10-28 20:49:32 +0000

    conditionally render menu only if there is content

--- src/EventPage/EventPage_Menu.jsx
+++ src/EventPage/EventPage_Menu.jsx
@@ -8,16 +8,25 @@ class EventPage_Menu extends Component {
   }
 
   render() {
-    return (
-      <div className="container-fluid menuContainer">
-        <h3>MENU</h3> 
-        <div className="row justify-content-center">
-          <div className="col-8">
-            {this.props.menu}
+    let menu;
+
+    if (this.props.menu) {
+      menu = (
+        <div className="container-fluid menuContainer">
+          <h3>MENU</h3>
+          <div className="row justify-content-center">
+            <div className="col-8">
+              {this.props.menu}
+            </div>
           </div>
         </div>
+      )
+    }
+
+    return (
+      <div>
+        { menu }
       </div>
-     
     );
   }
 }

0e790f6d590f3cafdef5c38b2732d159a8daf175
BugleJones <spencerjhamilton@gmail.com>
2017-10-31 19:03:22 +0000

    Commit before trying to create new navbarEvents set

--- src/NavBar/NavBar_PreviewEvent.jsx
+++ src/NavBar/NavBar_PreviewEvent.jsx
@@ -7,16 +7,27 @@ class NavBar_PreviewEvent extends Component {
   }
 
   render() {
-    return (
+    const hostSearch = (
       <div>
-        <Link to={`/events/${this.props.event_id}`} style={{ textDecoration: 'none' }}>
-          <div key={this.props.event_id} className="dropdown-item searchItem" data-event-id={this.props.event_id} onClick={this.handleClick} >
-            <b> {this.props.title} </b>
-            <div>{this.props.neighbourhood}</div>
+        <Link to={`/users/${this.props.host.user_id}`} style={{ textDecoration: 'none' }}>
+          <div key={this.props.host.user_id} className="dropdown-item searchItem" data-event-id={this.props.host.user_id}>
             <div>{this.props.host}</div>
           </div>
         </Link>
         <div className="dropdown-divider navsearch-divider"></div>
+      </div>
+    );
+    return (
+      <div>
+        <Link to={`/events/${this.props.event_id}`} style={{ textDecoration: 'none' }}>
+          <div key={this.props.event_id} className="dropdown-item searchItem" data-event-id={this.props.event_id}>
+            <b> {this.props.title} </b>
+            <div>{this.props.neighbourhood}</div>
+            <div>{this.props.price}</div>
+          </div>
+        </Link>
+        <div className="dropdown-divider navsearch-divider"></div>
+        { hostSearch }
       </div>
     )
   };

4fc6d0533db2b4c1bf37559dba1e787d093784ce
BugleJones <spencerjhamilton@gmail.com>
2017-11-01 17:08:10 +0000

    Added croissant logo and added styling to clean up the navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -110,7 +110,10 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top navbarContainer">
-        <a className="navbar-brand logo" href="/">Commune</a>
+        <div className="logo-bundle">
+          <img className="logo-photo" src={"../croissant_logo4.png"} href="/" alt="Commune Logo"></img>
+          <a className="navbar-brand logo" href="/">Commune</a>
+        </div>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
